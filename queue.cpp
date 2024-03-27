4ee396009d1a15fde97bfa40ae3b376fa6714501
Mohameddshawky <147840781+Mohameddshawky@users.noreply.github.com>
2024-03-27 15:58:49 +0200

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<class t>
+class Queue{
+struct node{
+    node*next{};
+    t value{};
+};
+int siz;
+node*first,*last;
+public:
+    Queue(){
+        first=last= nullptr;
+        siz=0;
+    }
+    bool isEmpty()const{
+        return siz==0;
+    }
+    int size()const{
+        return siz;
+    }
+    void enqueue(t element){
+        node*newn=new node;
+        newn->value=element;
+        if (siz==0){
+            first=last=newn;
+        }
+        else{
+            last->next=newn;
+            last=newn;
+        }
+        siz++;
+    }
+    void dequeue(){
+        if (!isEmpty()){
+            cout<<first->value<<"\n";
+            node*temp=first;
+            first=first->next;
+            delete temp;
+            siz--;
+        }
+        else{
+            cout<<"Queue is Empty\n";
+        }
+    }
+    void First()const{
+        if (!isEmpty()){
+            cout<<first->value<<"\n";
+        }
+        else{
+            cout<<"Queue is Empty\n";
+        }
+    }
+    void print()const{
+        node*temp=first;
+        while (temp!= nullptr){
+            cout<<temp->value<<" ";
+            temp=temp->next;
+        }
+        cout<<'\n';
+    }
+    void clear(){
+        node*temp=first;
+        siz=0;
+        while (first!= nullptr){
+            first=first->next;
+            delete temp;
+            temp=first;
+        }
+        delete first;
+    }
+};
+int main() {
+    Queue<string> q;
+    q.enqueue("mo");
+    q.enqueue("suii");
+    q.enqueue("shawky");
+    q.enqueue("za3lan");
+    q.print();
+    q.clear();
+    q.enqueue("ahmed");
+    q.print();
+    cout << q.size();
+    return 0;
+}

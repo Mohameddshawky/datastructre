e79db8325d245d99c8370f7f805ce36463764def
Mohameddshawky <147840781+Mohameddshawky@users.noreply.github.com>
2024-12-15 15:47:51 +0200

    Update suffixarray.cpp

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -1,81 +1,77 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
+const int N=1e5;
+bool compare(int i, int j, int rank[], int k, int length) {
+    if (rank[i] != rank[j])
+        return rank[i] < rank[j];
+    int ri = (i + k < length) ? rank[i + k] : -1;
+    int rj = (j + k < length) ? rank[j + k] : -1;
+    return ri < rj;
+}
+int partition(int arr[],int rank[],int l,int r,int k){
+    int i=l;
+    for (int j = i+1; j <=r ; ++j) {
+        if (compare(arr[j],arr[l],rank,k,r+1)){
+            swap(arr[j],arr[++i]);
+        }
+    }
+    swap(arr[l],arr[i]);
+    return i;
 
+}
+void quick_sort(int arr[],int rank[],int l,int r,int k){
+    if (l>=r)
+        return;
+    else{
+        int m= partition(arr,rank,l,r,k);
+        quick_sort(arr,rank,l,m-1,k);
+        quick_sort(arr,rank,m+1,r,k);
+    }
+}
 class SuffixArray {
 private:
-    string text;
+    char text[N];
     int length;
-    vector<int> suffixArray;
-    vector<int> rank;
-    vector<int> tempRank;
+    int suffixArray[N];
+    int rank[N];
+    int trank[N];
 public:
-    SuffixArray(const string &text) {
-        this->text = text;
-        this->length = text.size();
-        suffixArray.resize(length);
-        rank.resize(length);
-        tempRank.resize(length);
+    SuffixArray(const char* s) {
+        int cnt=0;
+        for (int i = 0; N ; ++i) {
+                text[i]=s[i];
+                cnt++;
+            if (s[i]=='$')break;
+        }
+        length=cnt;
     }
-    void createSuffixArray() {
+    void ConstructUsingPrefixDoubling() {
         for (int i = 0; i < length; i++) {
             suffixArray[i] = i;
-            rank[i] = text[i];
+            rank[i] = text[i]-'A';
         }
         for (int k = 1; k < length; k *= 2) {
-
-            auto compare = [this, k](int i, int j) {
-                if (rank[i] != rank[j])
-                    return rank[i] < rank[j];
-                int rank_i_k = (i + k < length) ? rank[i + k] : -1;
-                int rank_j_k = (j + k < length) ? rank[j + k] : -1;
-                return rank_i_k < rank_j_k;
-            };
-
-
-            sort(suffixArray.begin(), suffixArray.end(), compare);
-
-            tempRank[suffixArray[0]] = 0;
+            quick_sort(suffixArray,rank,0,length-1,k);
+            trank[suffixArray[0]] = 0;
             for (int i = 1; i < length; i++) {
-                tempRank[suffixArray[i]] = tempRank[suffixArray[i - 1]] +
-                                           (compare(suffixArray[i - 1], suffixArray[i]) ? 1 : 0);
+                trank[suffixArray[i]] = trank[suffixArray[i - 1]] +
+                                        (compare(suffixArray[i - 1], suffixArray[i],rank,k,length) ? 1 : 0);
             }
-            rank = tempRank;
+            for (int i = 0; i <length ; ++i) {
+                rank[i]=trank[i];
+            }
         }
     }
-
-    void printSuffixArray() {
+    void Print() {
         for (int i = 0; i < length; i++) {
-            cout << text.substr(suffixArray[i]) << "\t" << suffixArray[i] << endl;
+            cout << suffixArray[i] << " ";
         }
-    }
-    void Search(const string& temp){
-        int l=0,r=text.size(),mid;
-        while (r-l>1){
-            mid=(r+l)/2;
-            if (text.substr(suffixArray[mid], temp.size())<=temp)l=mid;
-            else
-                r=mid;
-        }
-        cout<<(temp==text.substr(suffixArray[l],temp.size())?"Yes":"No")<<"\n";
     }
 };
 
 int main() {
-    string text;
-    cin >> text;
-    SuffixArray suffixArray(text);
-    suffixArray.createSuffixArray();
-int q;cin>>q;
-    while (q--){
-        cin>>text;
-        suffixArray.Search(text);
-    }
-
-
-
+    SuffixArray t("ACGACTACGATAAC$");
+    t.ConstructUsingPrefixDoubling();
+    t.Print(); // Prints:  14 11 12  0  6  3  9 13  1  7  4  2  8 10  5
     return 0;
 }
